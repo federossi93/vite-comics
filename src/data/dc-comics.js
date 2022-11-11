c6844a26720188522e0b5429568b7626d4bf14b9
federossi93 <rossifederico0993@gmail.com>
2022-11-11 18:07:21 +0100

    fix file dc-comix.js

--- src/data/dc-comics.js
+++ src/data/dc-comics.js
@@ -1,5 +1,4 @@
-export default {
-  seriesCard:[
+export default[ 
     {
       thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
       price: "$19.99",
@@ -73,4 +72,4 @@ export default {
       type: "graphic novel"
     }
   ]
-}
+
